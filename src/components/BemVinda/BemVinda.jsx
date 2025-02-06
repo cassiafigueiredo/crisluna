e9290d0e72eb9eb197f40d21528b86392d135342
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2025-02-06 14:32:54 -0300

    mudanças seção inicial do site

--- src/components/BemVinda/BemVinda.jsx
+++ src/components/BemVinda/BemVinda.jsx
@@ -2,16 +2,19 @@ import styles from "../BemVinda/BemVinda.module.css"
 function BemVinda() {
     return(
         <>
-        <div className={`grid d-flex justify-content-center align-items-center   ${styles.containerBemVinda}`}> 
-            <div className={`row ${styles.elementos}`}>
-                <div className={`col-12 col-md-6`}>
-                    <img className={styles.imgBemVinda} src="./src/assets/images/design-entrada.png" alt="" />
+        <div className={` ${styles.containerBemVinda}`} >
+            <div className={`row align-items-center ${styles.containerBemVinda}`}>
+                <div className="col-md-6 d-none d-md-block">
+                    <img className={` ${styles.unhaimg}`} src="./src/assets/images/unhaprofissional.jpg" alt="unhas rosas" />
                 </div>
-                    <div className={`col-12 col-md-6 ${styles.txtBtn}`}>
-                        <h1>Cris Luna</h1>
-                        <p>Escolha seu bem-estar. Escolha Cris Luna.</p>
-                        <button className={styles.conhecerBtn}>Conhecer</button>
-                    </div>
+                <div className={`col-12 col-md-6 text-center text-md-left ${styles.txtBemVinda}`}>
+                    <h1 className={styles.h1CrisLuna}>
+                        Cris luna
+                    </h1>
+                    <p className={styles.pCrisLuna}>
+                        Cuidado, Charme & Capricho
+                    </p>
+                </div>
             </div>
         </div>
         </>
